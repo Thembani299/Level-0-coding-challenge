acf1f167d12239913bee5c23158ec1bfd33335d6
Thembani299 <104985388+Thembani299@users.noreply.github.com>
2022-05-21 15:48:40 +0200

    Add files via upload

---
+++ task_05.py
@@ -0,0 +1,4 @@
+def area_of_triangle(length1,length2,length3):
+    semiperimeter=1/2*(length1+length2+length3)
+    area=(semiperimeter*(semiperimeter-length1)*(semiperimeter-length2)*(semiperimeter-length3))**0.5
+    return area
