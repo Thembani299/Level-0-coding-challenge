b0bdd42427108c617d167e662be6123948f7c131
Thembani299 <104985388+Thembani299@users.noreply.github.com>
2022-05-23 07:29:46 +0200

    Delete Task 0.5.py

--- Task 0.5.py
+++
@@ -1,7 +0,0 @@
-from cmath import sqrt
-
-
-def area_of_triangle(length1,length2,length3):
-    semiperimeter=1/2*(length1+length2+length3)
-    area=sqrt(semiperimeter*(semiperimeter-length1)*(semiperimeter-length2)*(semiperimeter-length3))
-    return abs(area)
