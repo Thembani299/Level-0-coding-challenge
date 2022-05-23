2ee6c1ad9fc2583a611ed5f0b71d3dbd570e6ad5
Thembani299 <104985388+Thembani299@users.noreply.github.com>
2022-05-23 07:29:01 +0200

    Delete Task 0.7.py

--- Task 0.7.py
+++
@@ -1,7 +0,0 @@
-def celsius_to_fahrenheit(celsius):
-    fahrenheit=9*celsius/5+32
-    return fahrenheit
-
-def fahrenheit_to_celsius(fahrenheit):
-    celsius=(fahrenheit-32)*5/9
-    return celsius
