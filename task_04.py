47a37384ded488d5fdf12c7605d9b767c53ac242
Thembani299 <104985388+Thembani299@users.noreply.github.com>
2022-05-24 16:23:40 +0200

    Update task_04.py

--- task_04.py
+++ task_04.py
@@ -1,5 +1,8 @@
-def even_or_odd(integer):
+def even_or_odd():
+    print('Enter an integer')
+    integer=int(input())
     if integer%2==0:
         print("even")
     else:
         print("odd")
+even_or_odd()
