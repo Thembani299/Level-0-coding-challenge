320a795c8de48d0c50f75ad76478b6f67ca64207
Thembani299 <104985388+Thembani299@users.noreply.github.com>
2022-05-24 16:45:35 +0200

    Update task_07.py

--- task_07.py
+++ task_07.py
@@ -5,3 +5,5 @@ def celsius_to_fahrenheit(celsius):
 def fahrenheit_to_celsius(fahrenheit):
     celsius=(fahrenheit-32)*5/9
     return celsius
+print(celsius_to_fahrenheit(90))
+print(fahrenheit_to_celsius(194))
